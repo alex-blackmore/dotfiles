face3146e863e3df42e4203a555aed637e7e8239
Alex Blackmore <ablackmore113@gmail.com>
2026-06-11 12:50:36 +1000

    set different colorsheme

--- init.lua
+++ init.lua
@@ -4,6 +4,6 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.rnu = true
-
+vim.cmd.colorscheme("quiet")
 vim.api.nvim_set_hl(0, "Normal", { bg = "#1d1d1d" })
 
