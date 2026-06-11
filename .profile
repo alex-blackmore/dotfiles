84f2a8c6b9a0a7e61b872ac9914a52115ac2b852
Alex Blackmore <ablackmore113@gmail.com>
2026-06-11 12:45:06 +1000

    add my only two dotfiles

---
+++ .profile
@@ -0,0 +1,8 @@
+PS1='\u@\h \[\e[38;5;109m\]\w\n\[\e[0m\]\\$ '
+
+alias ,=ssh
+alias c='clear;~/motd'
+
+set -o vi
+
+~/motd
---
+++ init.lua
@@ -0,0 +1,9 @@
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.rnu = true
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "#1d1d1d" })
+
